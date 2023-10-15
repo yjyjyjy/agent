85f19b1fd417a7114345f66e5addc10682d847ff
yjyjyjy <prowessyang@gmail.com>
2023-10-14 19:01:39 -0700

    stringify

--- pages/api/webhooks/facebook/instagram.ts
+++ pages/api/webhooks/facebook/instagram.ts
@@ -10,7 +10,8 @@ export default async function handler(req, res) {
     if (mode === "subscribe" && token === 'e85d7e7a-053f-4347-9a6c-5eb2ba334df5') {
       // Respond with the challenge token from the request
       console.log("WEBHOOK_VERIFIED");
-      console.log('🔴 body', body)
+      console.log('🔴 req', req)
+      console.log('🔴 req', JSON.stringify(req))
       res.status(200).send(challenge);
     } else {
       // Respond with '403 Forbidden' if verify tokens do not match
