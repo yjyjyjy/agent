066585d68e87c054c4a468bafb40ca47b3236306
yjyjyjy <prowessyang@gmail.com>
2023-10-14 20:59:37 -0700

    save to supabase

--- pages/api/webhooks/facebook/instagram.ts
+++ pages/api/webhooks/facebook/instagram.ts
@@ -1,9 +1,17 @@
+
+import { supabase } from '@supabase/auth-ui-shared';
+import { createClient } from '@supabase/supabase-js'
+
+
 export default async function handler(req, res) {
+
+  const supabaseAdmin = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY)
   let mode = req.query["hub.mode"];
   let token = req.query["hub.verify_token"];
   let challenge = req.query["hub.challenge"];
   // Check if a token and mode is in the query string of the request
   console.log("🔴 agent received.body", JSON.stringify(req.body))
+  await supabaseAdmin.from('test').insert({ content: JSON.stringify(req.body) })
   // if (mode && token) {
   //   // Check the mode and token sent is correct
   //   if (mode === "subscribe" && token === 'e85d7e7a-053f-4347-9a6c-5eb2ba334df5') {
