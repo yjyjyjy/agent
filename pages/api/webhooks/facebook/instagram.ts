1e8e963f3d411b240a43e15cc071edd09badd409
yjyjyjy <prowessyang@gmail.com>
2023-10-14 18:37:32 -0700

    verification

--- pages/api/webhooks/facebook/instagram.ts
+++ pages/api/webhooks/facebook/instagram.ts
@@ -1,6 +1,21 @@
 export default async function handler(req, res) {
   const { body } = req;
-  console.log('🔴 body', body);
+
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+  // Check if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Check the mode and token sent is correct
+    if (mode === "subscribe" && token === 'e85d7e7a-053f-4347-9a6c-5eb2ba334df5') {
+      // Respond with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Respond with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
 
 
   res.status(200).send('EVENT_RECEIVED');
